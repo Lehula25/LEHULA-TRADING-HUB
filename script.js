6b5cbb1ada08ad5865e92344076c86649e9fcf74
Lehula25 <nthahamonaheng85@gmail.com>
2026-08-31 18:45:30 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const SUPABASE_URL = "https://qqavebqfrbkvbxsrtqke.supabase.co";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_PUBLISHABLE_KEY_HERE";
-
-const supabaseClient = window.supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_ANON_KEY
-);
-
-console.log("LEHULA Trading Hub connected to Supabase");
